a540dff5aeb41bc9c3a08a89df3590c49365b276
Ajay virkar <a>
2023-04-14 19:57:30 +0530

    Implemented the some part of comment-app

--- comments-app/src/components/Home.js
+++ comments-app/src/components/Home.js
@@ -73,16 +73,17 @@ function CommentsSection() {
     setCount(count - 1);
   }
 
-  const [newComment, setNewComment] = useState({ user: '', text: '' });
+  const [newComment, setNewComment] = useState({ username: " ", content: '', createdAt: '' });
 
   const handleChange = (e) => {
-    setNewComment({ ...newComment, [e.target.name]: e.target.value });
+    setNewComment({ ...newComment, content: e.target.value, username: "AjayVirkar", createdAt: "now" });
   };
+  // console.log("slzjdvnjldz", newComment)
 
   const handleAddComment = () => {
     const newId = comments.length + 1;
     setComments([...comments, { ...newComment, id: newId }]);
-    setNewComment('');
+    setNewComment({ content: "" });
   };
 
   const handleEditComment = (id) => {
@@ -113,80 +114,42 @@ function CommentsSection() {
   };
 
   return (
-    <div className='w-full h-screen mx-auto bg-gray-200 text-center'>
-      {comments.map((comment) => {
-        return (
-          <div key={comment.id} className='flex gap-4 w-1/2 mt-8 py-4 bg-white rounded-md mx-auto'>
-            <div className='flex flex-col px-3 m-5 justify-center bg-gray-200 rounded-md'>
-              <div onClick={increament} className='cursor-pointer'>+</div>
-              <div className='font-bold'>{count}</div>
-              <div onClick={decreament} className='cursor-pointer'>-</div>
-            </div>
-            <div className='p-2'>
-              <div className='flex justify-end'>
-                <button onClick={() => handleEditComment(comment.id)}><FiEdit2 /></button>
-                <button onClick={() => handleDeleteComment(comment.id)}><MdDelete /></button>
+    <div className='relative w-full h-screen mx-auto bg-gray-200 text-center'>
+      <p>Comments: </p>
+      <div className='h-[36rem] overflow-y-scroll'>
+        {comments.map((comment) => {
+          return (
+            <div key={comment.id} className='flex gap-4 w-1/2 mb-8 py-4 bg-white rounded-md mx-auto'>
+              <div className='flex flex-col px-3 m-5 justify-center bg-gray-200 rounded-md'>
+                <div onClick={increament} className='cursor-pointer'>+</div>
+                <div className='font-bold'>{count}</div>
+                <div onClick={decreament} className='cursor-pointer'>-</div>
               </div>
-              <div className='flex justify-between'>
-                <div className='flex justify-center items-center'>
-                  <img className='w-10 h-10' src={logo1} alt="logo" />
-                  <p className='mx-2 text-lg font-bold'>{comment.username}</p>
-                  <p className='text-gray-500'>{comment.createdAt}</p>
+              <div className='p-2'>
+                <div className='flex justify-end'>
+                  <button onClick={() => handleEditComment(comment.id)}><FiEdit2 /></button>
+                  <button onClick={() => handleDeleteComment(comment.id)}><MdDelete /></button>
                 </div>
-                <div className='flex flex-row gap-2 items-center'>
-                  <div className='text-xl font-bold text-blue-400'><IoArrowUndoSharp /></div>
-                  <div className='text-xl font-bold text-blue-400'>Reply</div>
+                <div className='flex justify-between'>
+                  <div className='flex justify-center items-center'>
+                    <img className='w-10 h-10' src={logo1} alt="logo" />
+                    <p className='mx-2 text-lg font-bold'>{comment.username}</p>
+                    <p className='text-gray-500'>{comment.createdAt}</p>
+                  </div>
+                  <div className='flex flex-row gap-2 items-center'>
+                    <div className='text-xl font-bold text-blue-400 cursor-pointer'><IoArrowUndoSharp /></div>
+                    <div className='text-xl font-bold text-blue-400 cursor-pointer'>Reply</div>
+                  </div>
                 </div>
+                <div className='text-start text-gray-500'>{comment.content}</div>
               </div>
-              <div className='text-start text-gray-500'>{comment.content}</div>
             </div>
-          </div>
-        )
-      })}
-      {/* {comments.map((comment) => (
-        <div key={comment.id}>
-          {comment.editing ? (
-            <div>
-              <input
-                type="text"
-                name="user"
-                value={comment.user}
-                onChange={(e) =>
-                  handleSaveComment(comment.id, {
-                    user: e.target.value,
-                    text: comment.text,
-                  })
-                }
-              />
-              <input
-                type="text"
-                name="text"
-                value={comment.text}
-                onChange={(e) =>
-                  handleSaveComment(comment.id, {
-                    user: comment.user,
-                    text: e.target.value,
-                  })
-                }
-              />
-              <button onClick={() => handleSaveComment(comment.id, comment)}>
-                Save
-              </button>
-            </div>
-          ) : (
-            <div>
-              <strong>{comment.user}: </strong>
-              {comment.text}
-              <button onClick={() => handleEditComment(comment.id)}><FiEdit2 /></button>
-              <button onClick={() => handleDeleteComment(comment.id)}>Delete</button>
-            </div>
-          )}
-        </div>
-      ))} */}
-      {/* <h2>Add a comment:</h2> */}
-      <div className='flex mt-12 flex-row justify-center gap-8 mx-auto ' >
+          )
+        })}
+      </div>
+      <div className='absolute bottom-0 w-full flex mt-12 flex-row justify-center gap-8 mx-auto ' >
         <div><img className='w-10 h-10' src={logo1} alt="image-logo" /></div>
-        <textarea className='w-1/3 h-20 rounded-md' type="text" name="text" value={newComment.text} onChange={handleChange} />
+        <textarea className='w-1/3 h-20 rounded-md' type="text" name="text" value={newComment.content} onChange={handleChange} />
         <button className='w-20 h-10 rounded-md bg-blue-500 text-xl text-white' onClick={handleAddComment}>Reply</button>
       </div>
     </div>
